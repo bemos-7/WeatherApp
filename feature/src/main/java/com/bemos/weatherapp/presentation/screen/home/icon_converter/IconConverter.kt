56a9887a8384bb321ab3f99a9533af8b1f74d302
bemos <bemo.0@mail.ru>
2024-07-09 19:21:29 +0300

    added some icons and weather checker

--- feature/src/main/java/com/bemos/weatherapp/presentation/screen/home/icon_converter/IconConverter.kt
+++ feature/src/main/java/com/bemos/weatherapp/presentation/screen/home/icon_converter/IconConverter.kt
@@ -18,16 +18,19 @@ class IconConverter {
                 "overcast" -> R.drawable.clouds
                 "patchy rain nearby" -> R.drawable.group_8
                 "cloudy" -> R.drawable.clouds
-                "heavy rain" -> R.drawable.clouds_rain
+                "heavy rain" -> R.drawable.heavy_rain_and_storm
                 "moderate rain" -> R.drawable.clouds_rain
                 "light rain shower" -> R.drawable.group_8
                 "fog" -> R.drawable.fog
                 "patchy light drizzle" -> R.drawable.clouds_rain
-                else -> { R.drawable.full_moon_second }
+                "light rain" -> R.drawable.light_rain
+                "patchy rain nearby" -> R.drawable.group_8
+                "light drizzle" -> R.drawable.light_drizzle
+                else -> { R.drawable.baseline_arrow_circle_down_24_s }
             }
         } else {
             return when(textCondition.toLowerCase().trim()) {
-                "sunny" -> R.drawable.sun
+                "sunny" -> R.drawable.full_moon_second
                 "partly cloudy" -> R.drawable.night
                 "clear" -> R.drawable.clear
                 "moderate or heavy rain with thunder" -> R.drawable.thunder
@@ -35,12 +38,15 @@ class IconConverter {
                 "overcast" -> R.drawable.clouds
                 "patchy rain nearby" -> R.drawable.night_rain
                 "cloudy" -> R.drawable.clouds
-                "heavy rain" -> R.drawable.clouds_rain
+                "heavy rain" -> R.drawable.heavy_rain_and_storm
                 "moderate rain" -> R.drawable.clouds_rain
                 "light rain shower" -> R.drawable.group_8
                 "fog" -> R.drawable.fog
                 "patchy light drizzle" -> R.drawable.clouds_rain
-                else -> { R.drawable.full_moon_second }
+                "light rain" -> R.drawable.light_rain
+                "patchy rain nearby" -> R.drawable.night_rain
+                "light drizzle" -> R.drawable.light_drizzle
+                else -> { R.drawable.baseline_arrow_circle_down_24_s }
             }
         }
 
