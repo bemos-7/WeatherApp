478c3fe217cde4f759b3474bb9eee289b633ea1f
bemos <bemo.0@mail.ru>
2024-07-31 23:49:54 +0300

    make change

--- data/src/main/java/com/bemos/data/remote/retrofit/weather/FetchApiKeyFirebase.kt
+++ data/src/main/java/com/bemos/data/remote/retrofit/weather/FetchApiKeyFirebase.kt
@@ -4,20 +4,26 @@ import android.util.Log
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
 import com.google.firebase.database.getValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 
 fun fetchApiKey(
     callback: (String?) -> Unit
 ) {
     val myRef = Firebase.database.getReference("api_key")
-
     myRef.get()
         .addOnSuccessListener { dataSnapshot ->
             val apiKey = dataSnapshot.getValue<String>()
             callback(apiKey)
         }
         .addOnFailureListener {
-            val error = it.message
+            val error = it
             Log.d("fetchApiKeyException", error.toString())
         }
 }
