a0d6f6432658a4a23cb7613004112a20f10870f0
bemos <bemo.0@mail.ru>
2024-08-12 15:31:04 +0300

    > override onBackround

--- app/src/main/java/com/bemos/weatherapp/ui/theme/Theme.kt
+++ app/src/main/java/com/bemos/weatherapp/ui/theme/Theme.kt
@@ -19,13 +19,15 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    onBackground = ContainerBlack
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    onBackground = ContainerWhite
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
