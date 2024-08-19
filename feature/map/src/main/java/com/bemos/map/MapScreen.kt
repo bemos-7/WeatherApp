d1f99b14b1fd4d01485f2633cb366845e9afc9a0
bemos <bemo.0@mail.ru>
2024-08-19 14:42:23 +0300

    > создал mapView

--- feature/map/src/main/java/com/bemos/map/MapScreen.kt
+++ feature/map/src/main/java/com/bemos/map/MapScreen.kt
@@ -3,6 +3,8 @@ package com.bemos.map
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -10,6 +12,7 @@ import com.bemos.feature.service.map.MapRepository
 import com.bemos.map.vm.MapScreenViewModel
 import com.bemos.map.vm.MapScreenViewModelFactory
 import com.bemos.shared.Constants.DETAILS_CITY
+import org.osmdroid.views.MapView
 
 @Composable
 fun MapScreen(
@@ -23,14 +26,17 @@ fun MapScreen(
         factory = mapScreenViewModelFactory
     )
 
-    val mapView by mapViewModel.mapViewGetter.collectAsState()
-
-
+    val context = LocalContext.current
+    val mapView = remember {
+        MapView(context)
+    }
 
     MapContent(
-        mapView = mapView!!,
+        mapView = mapView,
         onUserGeoPointClick = {
-            mapRepository.getUserGeoPoint()
+            mapRepository.getUserGeoPoint(
+                mapView
+            )
         },
         onMapClick = {
             navController.navigate("$DETAILS_CITY/${it}")
