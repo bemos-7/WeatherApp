c0bbc18ed5abcc6a569631672d5dd1f245d463fa
bemos <bemo.0@mail.ru>
2024-08-26 22:11:05 +0300

    > добавил иконок (много иконок)

--- feature/map/src/main/java/com/bemos/map/MapContent.kt
+++ feature/map/src/main/java/com/bemos/map/MapContent.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.bemos.map.utils.CustomMap
 import com.bemos.shared.R
 import org.osmdroid.events.MapEventsReceiver
